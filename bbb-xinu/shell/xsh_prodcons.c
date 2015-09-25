7fb5345de55b050afa9113aa4d74732795d613a4
Srivatsan <srriyer@indiana.edu>
2015-09-24 23:04:07 -0400

    Adding sanity checks for number parsing. Commit on behalf of Pralhad

--- bbb-xinu/shell/xsh_prodcons.c
+++ bbb-xinu/shell/xsh_prodcons.c
@@ -8,19 +8,30 @@
 int n;
 
 int parseNumber(char *str, int *ret, int max) {
-	*ret = atoi(str);
+	if (strnlen(str, max + 1) > max) {
+		return 0;
+	}
+	int i=0, len=strnlen(str, max+1);
+	int sum = 0;
+	for (i=0; i<len; i++) {
+		if (str[i] < '0' || str[i] > '9') {
+			return 0;
+		}
+		sum = sum * 10 + (str[i]- '0');
+	}
+	*ret = sum;
 	return 1;
 }
 
 shellcmd xsh_prodcons(int argc, char * argv[]) {
 	if (argc > 1 && argc != 2) {
-		fprintf(stderr, "%s: Expects only one parameter.", argv[0]);
+		fprintf(stderr, "%s: Expects only one parameter.\n", argv[0]);
 		return PRODCONS_EXIT_BAD_ARGS;
 	}
 	int count = 2000;
 	if (argc == 2) {
 		if (!parseNumber(argv[1], &count, 9)) {
-			fprintf(stderr, "%s: Unable to parse number. It should not have any more than 9 numeric characters.", argv[0]);
+			fprintf(stderr, "%s: Unable to parse number. It should not have any more than 9 numeric characters.\n", argv[0]);
 			return PRODCONS_EXIT_BAD_COUNT;
 		}
 	}
