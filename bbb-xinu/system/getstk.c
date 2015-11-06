c51e4ed32c01582566a13f907ab9c227b78fb263
Srivatsan <srriyer@indiana.edu>
2015-11-05 19:25:20 -0500

    Updated freemem/getmem/getstk. Removed print statements from initialize

--- bbb-xinu/system/getstk.c
+++ bbb-xinu/system/getstk.c
@@ -35,45 +35,11 @@ char  	*getstk(
 	
 		restore(mask);
 
-		if(++enable_kprintf_stack)
-			kprintf("Stacktop is %u\n", (uint32)stacktop);
+		/*if(++enable_kprintf_stack)
+			kprintf("Stacktop is %u\n", (uint32)stacktop);*/
 
 		return (char *)memptr;
 	}
-	else
-	{
-	}
-	/**
-	prev = &memlist;
-	curr = memlist.mnext;
-	fits = NULL;
-	fitsprev = NULL;  
-
-	while (curr != NULL) {			
-		if (curr->mlength >= nbytes) {	
-			fits = curr;		
-			fitsprev = prev;
-		}
-		prev = curr;
-		curr = curr->mnext;
-	}
-
-	if (fits == NULL) {			
-		restore(mask);
-		return (char *)SYSERR;
-	}
-	if (nbytes == fits->mlength) {		
-		fitsprev->mnext = fits->mnext;
-	} else {				
-		fits->mlength -= nbytes;
-		fits = (struct memblk *)((uint32)fits + fits->mlength);
-	}
-	memlist.mlength -= nbytes;
-
-	*/
-
 	restore(mask);
 	return (char *)SYSERR;
-
-	//return (char *)((uint32) fits + nbytes - sizeof(uint32));	
 }
